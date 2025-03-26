3a7cb36af3818fc6c7847beb4864c66fe6a806c7
marentsov <mr.marentsov@gmail.com>
2025-03-26 13:42:44 +0300

    update models

--- app/main/views.py
+++ app/main/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 
+from goods.models import Categories
 
 def index(request):
+    categories = Categories.objects.all()
     context = {
         'title': 'Home - главная',
-        'content': 'Магазин мебели HOME'
+        'content': 'Магазин мебели HOME',
+        'categories': categories
     }
     return render(
         request,
