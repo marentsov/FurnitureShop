dd8e59299c63cf8248bdb60e2dae56e41e67804f
marentsov <mr.marentsov@gmail.com>
2025-04-01 15:16:06 +0300

    add messages

--- app/users/views.py
+++ app/users/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.messages.context_processors import messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, reverse, redirect
 from django.contrib import auth
@@ -19,6 +20,7 @@ def login(request):
             user = auth.authenticate(username=username, password=password)
             if user:
                 auth.login(request, user)
+                messages.success(request, f"(username), Вы вошли в аккаунт!")
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
@@ -40,6 +42,7 @@ def registration(request):
             form.save()
             user = form.instance
             auth.login(request, user)
+            messages.success(request, f"(user.username), Вы успешно зарегистрировались")
             return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserRegistrationForm()
@@ -60,6 +63,7 @@ def profile(request):
         form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
         if form.is_valid():
             form.save()
+            messages.success(request, "Профиль обновлен")
             return HttpResponseRedirect(reverse('user:profile'))
     else:
         form = ProfileForm(instance=request.user)
@@ -76,6 +80,7 @@ def profile(request):
 
 @login_required
 def logout(request):
+    messages.success(request, f"(request.user.username), Вы успешно вышли из аккаунта"
     auth.logout(request)
     return redirect(reverse('main:index'))
 
