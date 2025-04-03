c7ec9d922493e0f86130c8db0a99d539e8db84b8
marentsov <mr.marentsov@gmail.com>
2025-04-03 08:23:33 +0300

    update cart for nonauth users

--- app/users/views.py
+++ app/users/views.py
@@ -5,6 +5,8 @@ from django.shortcuts import redirect, render, reverse
 
 from users.forms import ProfileForm, UserLoginForm, UserRegistrationForm
 
+from carts.models import Cart
+
 
 def login(request):
     if request.method == 'POST':
@@ -13,9 +15,15 @@ def login(request):
             username = request.POST['username']
             password = request.POST['password']
             user = auth.authenticate(username=username, password=password)
+
+            session_key = request.session.session_key
+
             if user:
                 auth.login(request, user)
                 messages.success(request, f"{username}, Вы вошли в аккаунт!")
+
+                if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
 
                 redirect_page = request.POST.get('next', None)
                 if redirect_page and redirect_page != reverse('user:logout'):
@@ -41,8 +49,15 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+
+            session_key = request.session_session_key
+
             user = form.instance
             auth.login(request, user)
+
+            if session_key:
+                Cart.objects.filter(session_key=session_key).update(user=user)
+
             messages.success(request, f"{user.username}, Вы успешно зарегистрировались")
             return HttpResponseRedirect(reverse('main:index'))
     else:
