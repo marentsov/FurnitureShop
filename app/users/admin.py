114807d8b2386f5d954942d47a0f37b8481ffc3e
marentsov <mr.marentsov@gmail.com>
2025-04-04 09:11:52 +0300

    add order creation

--- app/users/admin.py
+++ app/users/admin.py
@@ -4,13 +4,15 @@ from users.models import User
 
 from carts.admin import CartTabAdmin
 
+from orders.admin import OrderTabulareAdmin
+
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ["username", "last_name", "first_name", "email",]
     search_fields = ["username", "last_name", "first_name", "email",]
 
-    inlines = [CartTabAdmin,]
+    inlines = [CartTabAdmin, OrderTabulareAdmin]
 
 
 
